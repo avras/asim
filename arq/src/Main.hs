574546615b1f2d6423c47810e84bb41279def481
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-03-31 16:13:59 +0530

    Added sequence number checking and change event queue to sorted list

--- arq/src/Main.hs
+++ arq/src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad.RWS
-import qualified Data.PQueue.Prio.Min as PQ
+import qualified Data.SortedList as SL
 
 type Time = Double
 type Probability = Double
@@ -18,8 +18,7 @@ data ARQSimConfig = ARQSimConfig {
 data ARQSimLog = ARQSimLog {
   numPacketsTransmitted   :: Int,
   numACKTimeouts          :: Int,
-  numACKsTransmitted      :: Int,
-  numDupPacketsReceived   :: Int
+  numACKsTransmitted      :: Int
 } deriving (Show)
 
 instance Monoid ARQSimLog where
@@ -27,59 +26,80 @@ instance Monoid ARQSimLog where
   log1 `mappend` log2 = ARQSimLog {
       numPacketsTransmitted = numPacketsTransmitted log1 + numPacketsTransmitted  log2,
       numACKTimeouts        = numACKTimeouts        log1 + numACKTimeouts         log2,
-      numACKsTransmitted    = numACKsTransmitted    log1 + numACKsTransmitted     log2,
-      numDupPacketsReceived = numDupPacketsReceived log1 + numDupPacketsReceived  log2
+      numACKsTransmitted    = numACKsTransmitted    log1 + numACKsTransmitted     log2
     }
 
 logEmpty = ARQSimLog {
   numPacketsTransmitted = 0,
   numACKTimeouts        = 0,
-  numACKsTransmitted    = 0,
-  numDupPacketsReceived = 0
+  numACKsTransmitted    = 0
 }
 
 onePacketTransmitted  = logEmpty { numPacketsTransmitted  = 1 }
 onePacketTimeout      = logEmpty { numACKTimeouts         = 1 }
 oneACKTransmitted     = logEmpty { numACKsTransmitted     = 1 }
-oneDupPacketReceived  = logEmpty { numDupPacketsReceived  = 1 }
 
 data SeqNum = Zero | One
+  deriving (Eq, Show)
+
+snflip :: SeqNum -> SeqNum
+snflip Zero = One
+snflip One = Zero
+
 
 data ARQTransmitterState = ARQTransmitterState {
-  currentPacketSeqNum     :: SeqNum,
-  packetTimeoutEvent      :: Event
+  expectedACKSeqNum       :: SeqNum,
+  ackTimeoutEvent         :: Maybe TimedEvent
 }
 
 data ARQReceiverState = ARQReceiverState {
-  currentAckSeqNum        :: SeqNum
+  expectedPacketSeqNum    :: SeqNum
 }
 
-type EventQueue = PQ.MinPQueue Time Event
+data TimedEvent = TE Time Event
+
+instance Eq TimedEvent where
+  (TE t1 _) == (TE t2 _) = t1 == t2
+
+instance Ord TimedEvent where
+  (TE t1 _) `compare` (TE t2 _) = t1 `compare` t2
+
+type EventQueue = SL.SortedList TimedEvent
 
 data ARQSimState = ARQSimState {
   currentSimulationTime   :: Time,
+  currentPacketSeqNum     :: SeqNum,
   transmitterState        :: ARQTransmitterState,
+  currentACKSeqNum        :: SeqNum,
   receiverState           :: ARQReceiverState,
   eventQueue              :: EventQueue
 }
 
 initialTransmitterState = ARQTransmitterState {
-  currentPacketSeqNum = Zero,
-  packetTimeoutEvent     = transmitPacket
+  expectedACKSeqNum       = One,
+  ackTimeoutEvent         = Nothing
 }
 
 initialReceiverState = ARQReceiverState {
-  currentAckSeqNum = One
+  expectedPacketSeqNum    = Zero
 }
 
 initialState = ARQSimState {
-  currentSimulationTime = 0.0,
-  transmitterState      = initialTransmitterState,
-  receiverState         = initialReceiverState,
-  eventQueue            = PQ.singleton 0.0 transmitPacket
+  currentSimulationTime   = 0.0,
+  currentPacketSeqNum     = Zero,
+  transmitterState        = initialTransmitterState,
+  currentACKSeqNum        = Zero,
+  receiverState           = initialReceiverState,
+  eventQueue              = SL.singleton $ TE 0.0 transmitPacket
 }
 
 type Event = RWST ARQSimConfig ARQSimLog ARQSimState IO ()
+
+setTransmitterState :: ARQTransmitterState -> ARQSimState -> ARQSimState
+setTransmitterState txst st = st { transmitterState = txst }
+
+setReceiverState :: ARQReceiverState -> ARQSimState -> ARQSimState
+setReceiverState rxst st = st { receiverState = rxst }
 
 transmitPacket :: Event
 transmitPacket = do
@@ -87,13 +107,31 @@ transmitPacket = do
   t <- gets currentSimulationTime
   liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: Packet transmitted"
   fd <- asks forwardPropDelay
-  modify $ scheduleEvent (t+fd) receivePacket
+  modify . scheduleEvent $ TE (t+fd) receivePacket
+  td <- asks timeoutDuration
+  let timeoutEv = TE (t+td) ackTimeout
+  modify $ scheduleEvent timeoutEv
+  txst <- gets transmitterState
+  modify . setTransmitterState $ txst { ackTimeoutEvent = Just timeoutEv }
+
 
 receivePacket :: Event
 receivePacket = do
   t <- gets currentSimulationTime
   liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: Packet received"
-  transmitACK
+  psn <- gets currentPacketSeqNum
+  rxst <- gets receiverState
+  let epsn = expectedPacketSeqNum rxst
+  case psn == epsn of
+    True -> do
+      liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: Sequence number in received packet = " 
+        ++ (show psn) ++ ". Expected packet sequence number = " ++ (show epsn) ++ "."
+      modify flipACKSeqNum
+      modify . setReceiverState $ rxst { expectedPacketSeqNum = snflip epsn }
+      transmitACK
+    False ->
+      liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: ERROR: Sequence number in received packet = " 
+        ++ (show psn) ++ ". Expected packet sequence number = " ++ (show epsn) ++ "."
 
 ackTimeout :: Event
 ackTimeout = do
@@ -108,25 +146,50 @@ transmitACK = do
   t <- gets currentSimulationTime
   liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: ACK transmitted"
   rd <- asks reversePropDelay
-  modify $ scheduleEvent (t+rd) receiveACK
+  modify . scheduleEvent $ TE (t+rd) receiveACK
 
 receiveACK :: Event
 receiveACK = do
   t <- gets currentSimulationTime
   liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: ACK received"
-  transmitPacket
+  asn <- gets currentACKSeqNum
+  txst <- gets transmitterState 
+  let easn = expectedACKSeqNum txst
+  case easn == asn of
+    True -> do
+      liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: Sequence number in received ACK = "
+        ++ (show asn) ++ ". Expected ACK sequence number = " ++ (show easn) ++ "."
+      modify flipPacketSeqNum
+      modify . setTransmitterState $ txst { expectedACKSeqNum = snflip easn }
+      case ackTimeoutEvent txst of
+        Nothing -> transmitPacket
+        Just timeoutEv -> do
+          modify $ cancelEvent timeoutEv
+          transmitPacket
+    False -> do
+      liftIO . putStrLn $ "t = " ++ (show t) ++ " seconds: ERROR: Sequence number in received ACK = "
+        ++ (show asn) ++ ". Expected ACK sequence number = " ++ (show easn) ++ "."
 
-scheduleEvent :: Time -> Event -> ARQSimState -> ARQSimState
-scheduleEvent t event st =
-  st { eventQueue = PQ.insert t event $ eventQueue st }
+scheduleEvent :: TimedEvent -> ARQSimState -> ARQSimState
+scheduleEvent te st =
+  st { eventQueue = SL.insert te $ eventQueue st }
+
+cancelEvent :: TimedEvent -> ARQSimState -> ARQSimState
+cancelEvent te st =
+  st { eventQueue = SL.delete te $ eventQueue st }
+
+flipPacketSeqNum :: ARQSimState -> ARQSimState
+flipPacketSeqNum st = st { currentPacketSeqNum = snflip $ currentPacketSeqNum st }
+
+flipACKSeqNum :: ARQSimState -> ARQSimState
+flipACKSeqNum st = st { currentACKSeqNum = snflip $ currentACKSeqNum st }
 
 arqSim :: Event
 arqSim = do
   q <- gets eventQueue      -- Get the current eventQueue
-  case PQ.null q of         -- Check if the eventQueue is empty
-    True -> return ()
-    False -> do
-      let ((t, event), q') = PQ.deleteFindMin q   -- Get the earliest event in the eventQueue
+  case SL.uncons q of         -- Check if the eventQueue is empty
+    Nothing -> return ()
+    Just (TE t event, q') -> do
       endt <- asks simDuration  -- Get the simulation duration
       case (t > endt) of        -- Check if earliest event time exceeds simulation duration
         True -> return ()
